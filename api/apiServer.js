628485d46a75ec410ac4d1766ccf946698e86e48
Lukasz <skotniczny@outlook.com>
2022-04-03 22:14:59 +0200

    Migrate from request to got

--- api/apiServer.js
+++ api/apiServer.js
@@ -1,8 +1,8 @@
-const http = require('http')
-const cors = require('cors')
-const Express = require('express')
+import http from 'http'
+import cors from 'cors'
+import Express from 'express'
 
-const { apiRequest, processRequest } = require('./utils/helpers.js')
+import { apiRequest } from './utils/helpers.js'
 
 const app = new Express()
 const server = new http.Server(app)
@@ -16,40 +16,34 @@ app.use((req, res, next) => {
   next()
 })
 
-app.get('/stopPoints/:query', (req, res) => {
-  apiRequest(api + new Date().getTime(), 'getStopPoints', { pattern: decodeURIComponent(req.params.query) }, (err, httpResponse, body) => {
-    processRequest(err, body, res)
-  })
+app.get('/stopPoints/:query', async (req, res) => {
+  const data = await apiRequest(api + new Date().getTime(), 'getStopPoints', { pattern: decodeURIComponent(req.params.query) })
+  res.json(data.success)
 })
 
-app.get('/streets/:query', (req, res) => {
-  apiRequest(api + new Date().getTime(), 'getStreets', { pattern: decodeURIComponent(req.params.query) }, (err, httpResponse, body) => {
-    processRequest(err, body, res)
-  })
+app.get('/streets/:query', async (req, res) => {
+  const data = await apiRequest(api + new Date().getTime(), 'getStreets', { pattern: decodeURIComponent(req.params.query) })
+  res.json(data.success)
 })
 
-app.get('/bollardsByStopPoint/:name', (req, res) => {
-  apiRequest(api + new Date().getTime(), 'getBollardsByStopPoint', { name: decodeURIComponent(req.params.name)}, (err, httpResponse, body) => {
-    processRequest(err, body, res)
-  })
+app.get('/bollardsByStopPoint/:name', async (req, res) => {
+  const data = await apiRequest(api + new Date().getTime(), 'getBollardsByStopPoint', { name: decodeURIComponent(req.params.name) })
+  res.json(data.success)
 })
 
-app.get('/bollardsByStreet/:name', (req, res) => {
-  apiRequest(api + new Date().getTime(), 'getBollardsByStreet', { name: decodeURIComponent(req.params.name) }, (err, httpResponse, body) => {
-    processRequest(err, body, res)
-  })
+app.get('/bollardsByStreet/:name', async (req, res) => {
+  const data = await apiRequest(api + new Date().getTime(), 'getBollardsByStreet', { name: decodeURIComponent(req.params.name) })
+  res.json(data.success)
 })
 
-app.get('/times/:bollard', (req, res) => {
-  apiRequest(api + new Date().getTime(), 'getTimes', { symbol: req.params.bollard }, (err, httpResponse, body) => {
-    processRequest(err, body, res)
-  })
+app.get('/times/:bollard', async (req, res) => {
+  const data = await apiRequest(api + new Date().getTime(), 'getTimes', { symbol: req.params.bollard })
+  res.json(data.success)
 })
 
-app.get('/bollardsByLine/:line', (req, res) => {
-  apiRequest(api + new Date().getTime(), 'getBollardsByLine', { name: req.params.line }, (err, httpResponse, body) => {
-    processRequest(err, body, res)
-  })
+app.get('/bollardsByLine/:line', async (req, res) => {
+  const data = await apiRequest(api + new Date().getTime(), 'getBollardsByLine', { name: req.params.line })
+  res.json(data.success)
 })
 
 server.listen(port, (err) => {
--- api/utils/helpers.js
+++ api/utils/helpers.js
@@ -1,24 +1,20 @@
-const request = require('request')
+import got from 'got'
 
-function apiRequest(url, method, object, callback) {
-  request
-    .post({
-      url: url,
-      form: {
-        method: method,
-        p0: JSON.stringify(object)
-      },
-    }, callback)
-    .setHeader('Content-type', 'application/x-www-form-urlencoded; charset=UTF-8')
-}
-
-function processRequest(err, body, res) {
-  if (err) {
-    return console.error('Błąd:', err)
+export async function apiRequest(url, method, object) {
+  try {
+    return await got
+      .post({
+        url: url,
+        form: {
+          method: method,
+          p0: JSON.stringify(object)
+        },
+        headers: {
+          'Content-type': 'application/x-www-form-urlencoded; charset=UTF-8'
+        }
+      })
+  } catch (error) {
+    console.log('Błąd: ', error)
   }
-  data = JSON.parse(body)
-  res.json(data.success)  
-}
 
-exports.apiRequest = apiRequest
-exports.processRequest = processRequest
+}
