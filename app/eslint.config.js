d806c03b3f5f779c6a2b2ea169e925fe8f459f01
Łukasz <skotniczny@outlook.com>
2025-02-04 21:52:01 +0100

    Add nodeBuiltin globals to eslint.config.js

--- app/eslint.config.js
+++ app/eslint.config.js
@@ -12,7 +12,7 @@ export default [
       ecmaVersion: 2020,
       globals: {
         ...globals.browser,
-        ...globals.nodeBuiltin
+        ...globals.nodeBuiltin,
       },
       parserOptions: {
         ecmaVersion: 'latest',
