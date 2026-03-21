2eeb3f7f1a77ede0ba2929f80a0bef16bd095d81
Łukasz <skotniczny@outlook.com>
2026-03-21 14:14:18 +0100

    Replace chained setTimeout with setInterval in Clock component

--- app/src/components/Clock.jsx
+++ app/src/components/Clock.jsx
@@ -4,14 +4,12 @@ import { useEffect } from 'react';
 
 const Clock = () => {
   const [date, setDate] = useState(new Date());
-  const tick = () => {
-    setDate(new Date())
-  }
+  const tick = () => setDate(new Date());
 
   useEffect(() => {
-    const id = setTimeout(() => (tick(), 1000));
-    return () => clearTimeout(id)
-  }, [date]);
+    const id = setInterval(tick, 1000);
+    return () => clearInterval(id);
+  }, []);
 
   const offset = date.getTimezoneOffset() * 60000;
   const localDateISOString = (new Date(date - offset)).toISOString();
