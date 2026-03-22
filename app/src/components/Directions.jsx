e170f43a73ba109d55e8c7c30788a585eef93254
Łukasz <skotniczny@outlook.com>
2026-03-22 14:06:36 +0100

    Add ARIA roles to Directions

--- app/src/components/Directions.jsx
+++ app/src/components/Directions.jsx
@@ -1,14 +1,14 @@
 const Directions = ({ directions }) => {
   return (
-    <div>
-      <div className="stop-point__headings">
-        <div className="item-property item-line">Linia</div>
-        <div className="item-property item-direction">Kierunek</div>
+    <div role="table">
+      <div className="stop-point__headings" role="row">
+        <div className="item-property item-line" role="columnheader">Linia</div>
+        <div className="item-property item-direction" role="columnheader">Kierunek</div>
       </div>
       {directions.map((item, index) => (
-        <div className="stop-point__item" key={index}>
-          <div className="item-property item-line">{item.lineName}</div>
-          <div className="item-property item-direction">{item.direction}</div>
+        <div className="stop-point__item" role="row" key={index}>
+          <div className="item-property item-line" role="cell">{item.lineName}</div>
+          <div className="item-property item-direction" role="cell">{item.direction}</div>
         </div>
       ))}
     </div>
