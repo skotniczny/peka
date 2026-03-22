b04155e28985725b65a742b09585b5ae90cabdfd
Łukasz <skotniczny@outlook.com>
2026-03-22 20:45:27 +0100

    Use semantic list markup in SearchResults

--- app/src/components/SearchResults.jsx
+++ app/src/components/SearchResults.jsx
@@ -4,15 +4,15 @@ const SearchResults = ({ result, isEmpty, path }) => {
   if (!result.length || isEmpty) return null;
 
   return (
-    <div className="results-list">
+    <ul className="results-list">
       {result.map((item, index) => (
-        <div className="search-row" key={`${path}-${index}`}>
+        <li className="search-row" key={`${path}-${index}`}>
           <Link to={`/${path}/${encodeURIComponent(item.name)}`} className="search-row-link">
             {item.name} {(item.symbol ? <span className="tag">{item.symbol}</span> : "")}
           </Link>
-        </div>
+        </li>
       ))}
-    </div>
+    </ul>
   );
 }
 
