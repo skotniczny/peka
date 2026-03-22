9a6453e215e9448884d1496b4769e77e066e0ded
Łukasz <skotniczny@outlook.com>
2026-03-22 18:58:35 +0100

    Remove empty no-results div from SearchResults

--- app/src/components/SearchResults.jsx
+++ app/src/components/SearchResults.jsx
@@ -1,21 +1,19 @@
 import { Link } from 'react-router-dom';
 
 const SearchResults = ({ result, isEmpty, path }) => {
-  if (result.length && !isEmpty) {
-    return (
-      <div className="results-list">
-        {result.map((item, index) => (
-          <div className="search-row" key={`${path}-${index}`}>
-            <Link to={`/${path}/${encodeURIComponent(item.name)}`} className="search-row-link">
-              {item.name} {(item.symbol ? <span className="tag">{item.symbol}</span> : "")}
-            </Link>
-          </div>
-        ))}
-      </div>
-    );
-  } else {
-    return (<div className="no-results"></div>);
-  }
+  if (!result.length || isEmpty) return null;
+
+  return (
+    <div className="results-list">
+      {result.map((item, index) => (
+        <div className="search-row" key={`${path}-${index}`}>
+          <Link to={`/${path}/${encodeURIComponent(item.name)}`} className="search-row-link">
+            {item.name} {(item.symbol ? <span className="tag">{item.symbol}</span> : "")}
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default SearchResults;
