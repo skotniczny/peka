5f145cf84c91d146c7f7a7bf409b99eafd7f582b
Łukasz <skotniczny@outlook.com>
2026-03-24 18:00:32 +0100

    Add role and label to Spinner for accessibility

--- app/src/components/Spinner.jsx
+++ app/src/components/Spinner.jsx
@@ -1,5 +1,5 @@
 const Spinner = () => (
-  <div className="spinner">
+  <div role="status" aria-label="Ładowanie" className="spinner">
     <div className="bounce1"></div>
     <div className="bounce2"></div>
     <div className="bounce3"></div>
