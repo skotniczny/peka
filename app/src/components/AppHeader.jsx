313b653b2cb4b7e33cc09f27777116a80df6c47e
Łukasz <skotniczny@outlook.com>
2026-03-28 12:58:30 +0100

    Add hamburger menu to toggle search on mobile in AppHeader

--- app/src/components/AppHeader.jsx
+++ app/src/components/AppHeader.jsx
@@ -1,20 +1,33 @@
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 import Clock from './Clock';
 import Search from './Search';
 
-const AppHeader = () => (
-  <header className="app-header">
+const AppHeader = () => {
+  const [open, setOpen] = useState(false);
+  return (<header className="app-header">
     <div className="app-title">
       <h1 className="app-name"><Link className="app-name-link" to="/">Wirtualny Monitor</Link></h1>
+      <button 
+        type="button"
+        className="btn mobile-only"
+        aria-controls="app-search"
+        aria-expanded={open}
+        aria-label={open ? "Zamknij menu wyszukiwania" : "Otwórz menu wyszukiwania"}
+        onClick={() => setOpen(!open)}>
+          <span aria-hidden="true">☰</span>
+      </button>
       <Clock />
     </div>
     <div className="header-description">
       <p>Monitor pokazuje rzeczywisty czas przyjazdu tramwaju lub autobusu na konkretny przystanek.</p>
       <p>Znajdź linię, która cię interesuje oraz przystanek z którego chcesz jechać, a system pokaże rzeczywisty czas przyjazdu.</p>
     </div>
-    <Search config={{ method: 'stopPoints', label: 'przystanki', path: 'przystanki', placeholder: 'Wpisz nazwę przystanku' }} />
-    <Search config={{ method: 'streets', label: 'ulice', path: 'ulica', placeholder: 'Wpisz ulicę lub miejscowość' }} />
-  </header>
-)
+    <div id="app-search" className={`app-search ${open ? "app-search_open" : ""}`}>
+      <Search config={{ method: 'stopPoints', label: 'przystanki', path: 'przystanki', placeholder: 'Wpisz nazwę przystanku' }} />
+      <Search config={{ method: 'streets', label: 'ulice', path: 'ulica', placeholder: 'Wpisz ulicę lub miejscowość' }} />
+    </div>
+  </header>);
+}
 
 export default AppHeader;
